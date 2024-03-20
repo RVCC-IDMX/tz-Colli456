eaf41965590fb5ca6f423363639ee3d5d8249dd5
Colli456 <cjclarke14y@gmail.com>
2024-03-19 20:27:21 -0400

    Add Format

--- tz.js
+++ tz.js
@@ -1,14 +1,23 @@
+/* eslint-disable import/no-extraneous-dependencies */
 const moment = require('moment-timezone');
+
+const yargs = require('yargs');
+
+console.log(yargs.argv);
 
 moment.tz.setDefault('America/New_York');
 
 let targetTimezone;
 
-if (process.argv.length != 3) {
+if (yargs.argv._.length !== 1) {
   console.log('Usage: node <script-file> <timezone>');
   process.exit(1);
 } else {
-  targetTimezone = process.argv[2];
+  targetTimezone = yargs.argv._[0];
 }
 
-console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
+if (yargs.argv.format === true) {
+  console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format('dddd, MMMM Do YYYY, h:mm:ss a')}`);
+} else {
+  console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
+}
